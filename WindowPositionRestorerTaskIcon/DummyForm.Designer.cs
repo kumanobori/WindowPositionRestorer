8e8a2853555d4ccdb08613d5af35a0a81a4b80aa
kumanobori <jirosv01@gmail.com>
2021-06-12 11:57:09 +0900

    アイコン変更

--- WindowPositionRestorerTaskIcon/DummyForm.Designer.cs
+++ WindowPositionRestorerTaskIcon/DummyForm.Designer.cs
@@ -46,6 +46,7 @@ namespace WindowPositionRestorerTaskIcon
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DummyForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- WindowPositionRestorerTaskIcon/Program.cs
+++ WindowPositionRestorerTaskIcon/Program.cs
@@ -19,7 +19,7 @@ namespace WindowPositionRestorerTaskIcon
                 Application.SetHighDpiMode(HighDpiMode.SystemAware);
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                //Application.Run(new DummyForm());
+                // Application.Run(new DummyForm());
                 Form f = new DummyForm();
                 Application.Run();
             }
